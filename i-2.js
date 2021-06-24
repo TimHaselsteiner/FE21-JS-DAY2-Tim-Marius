970afd7e535e1cdf5c6a005d283f23bd5d5210fd
Tim  Haselsteiner <haselsteiner.t00@gmail.com>
2021-06-24 14:10:00 +0200

    Intermediate 2

---
+++ i-2.js
@@ -0,0 +1,60 @@
+// var Mathe = 3;
+// var Physik = 4;
+// var Englisch = 5;
+
+// function calculations(Mathe,Physik,Englisch){
+
+//     var Sum = Mathe + Physik + Englisch;
+
+//     return Sum;
+
+//     var Avg = (Mathe + Physik + Englisch)/3;
+
+    
+
+    
+// }
+
+
+
+
+// document.write(calculations);
+
+// //var Sum = (Mathe+Physik+Englisch);
+// // var Avg = ((Mathe+Phsyik+Englisch)/3);
+
+
+
+
+
+function calc2(mathe, physik, english) {
+    var multi = ((mathe + physik + english)/3);
+
+    return multi;
+
+    
+
+
+
+
+}
+document.write("The average is: " + calc2(3, 2, 5) +  "<br>")
+
+
+
+
+
+function calc3(mathe1, physik1, english1) {
+    var sum = (mathe1 + physik1 + english1);
+
+    return sum;
+
+   
+
+
+
+
+}
+
+
+document.write("The Sum is : " + calc3(3,2,5))
