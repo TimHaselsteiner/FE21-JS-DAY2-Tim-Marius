0cedb376c66146a48e4ccc0dbbe07a6e79592fa3
Marius Caamano <marius.caamano@hotmail.com>
2021-06-24 13:36:21 +0200

    marius add int. files

---
+++ intermediate.js
@@ -0,0 +1,22 @@
+(function (text1) {
+    let game = "i am a game developer"
+    console.log(game.charAt(0).toUpperCase() + game.substring(1,25))
+})("Marius")
+
+
+
+
+
+    
+
+
+var myTraining = "i am a web developer";
+
+
+var res = myTraining.charAt(0).toUpperCase();
+
+console.log(res + myTraining.substring(1,20))
+
+
+
+
